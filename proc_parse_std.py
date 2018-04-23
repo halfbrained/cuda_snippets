38c91dd85bfffb9e6cc69e8c6c014718614dcd7e
Alexey-t <support@uvviewsoft.com>
2018-04-23 10:46:29 +0300

    refc

--- proc_parse_std.py
+++ proc_parse_std.py
@@ -5,8 +5,8 @@ SNIP_LEX='lex'
 SNIP_TEXT='text'
 
 def parse_snippet_file(text):
-    res={SNIP_NAME:'', SNIP_ID:'', SNIP_LEX:'', SNIP_TEXT:'' }
-    lines=text.splitlines()
+    res = {SNIP_NAME: '', SNIP_ID: '', SNIP_LEX: '', SNIP_TEXT: [] }
+    lines = text.splitlines()
 
     for (index, line) in enumerate(lines):
         if line==SNIP_TEXT+'=':
