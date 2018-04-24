0570d3e37c128c6d8159a33bf6d7e20445f56f37
Andrey Kvichanskiy <kvichans@mail.ru>
2018-04-24 11:16:19 +0300

    Allow empty id. Return None for broken format .

--- proc_parse_simple.py
+++ proc_parse_simple.py
@@ -2,10 +2,11 @@ import re
 
 def parse_simple_snippet_line(line):
     """ Parse one-line snippet definition
-            word [/L="My Lexer"] [/N="My Name"] text text
+            [word ][/L="My Lexer"] [/N="My Name"] text text
+        If word is omitted then /N= is required.
         Quotes around lexer/name are required if value has blanks.
         Text part can has \t \n \r \\ to include chr(9) chr(10) chr(13) and '\' .
-        Min line need to have two words.
+        Minimum line need to have two words or /N=name and text .
         Return
             {'id'  :'word',
              'name':'My Name',  # 'name':'word' if no /N= in line
@@ -13,9 +14,12 @@ def parse_simple_snippet_line(line):
              'text':['text text']
             }
     """
-    key     = line.split()[0]
-    line    = line[len(key):].lstrip()
+    if len(line.split()) < 2:
+        return None
 
+    key     = line.split()[0]   if  line[:3] not in ('/N=', '/L=') else ''
+    line    = line[len(key):].lstrip()
+                
     def opt_val(line, opt, defv):
         optv    = defv
         opt     = opt if opt[0]=='/' else '/'+opt+'='
@@ -30,7 +34,7 @@ def parse_simple_snippet_line(line):
                 optv    = mtch.group(1)
                 line    = line.replace(opt+optv, '').lstrip()
         return optv,line
-
+                
     name    = key
     lex     = ''
     if line[:3]=='/N=':
@@ -40,8 +44,15 @@ def parse_simple_snippet_line(line):
     if line[:3]=='/N=':
         name,line   = opt_val(line, 'N', defv=name)
 
+    if not key and not name:
+        return None
+                
     body    = line.lstrip()
     body    = body.replace('\\\\', chr(0))
     body    = body.replace('\\t', chr(9)).replace('\\n', chr(10)).replace('\\r', chr(13))
     body    = body.replace(chr(0), '\\')
+
+    if not body:
+        return None
+
     return {'id':key, 'name':name, 'lex':lex, 'text':body.splitlines() }
